e01922c5ad16c0756b9e2fe243d5bf28765bf9c1
Ernest <ernestwinata@gmail.com>
2023-03-10 11:55:27 -0800

    creates myfile.md

---
+++ index.js
@@ -0,0 +1,46 @@
+function add() {
+}
+function subtract() {
+}
+function multiply() {
+}
+function divide() {
+}
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a*b;
+}
+function divide(a, b) {
+    return a/b;
+}
+function increment(a) {
+    return a + 1;
+}
+function decrement(a) {
+    return a - 1;
+}
+function makeInt(n) {
+    return(parseInt(n, 10));
+}
+function makeInt(n) {
+    number = n
+    parseInt("0*2328", 10);
+    return number;
+}
+function makeInt(n) {
+    parseInt("sldkjflksjf", 10);
+    return (n);
+}
+function preserveDecimal(n) {
+    number = parseFloat(n, 10);
+    return number;
+}
+function preserveDecimal(n) {
+    parseFloat("sldkjflksjf", "sldkjflksjf");
+    return (n);
+}
